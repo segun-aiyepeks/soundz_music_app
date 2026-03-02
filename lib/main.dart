aef7403d330213c9b0b0e52c3c9d75e4fab72e14
segun-aiyepeks <your-email@example.com>
2026-03-02 10:10:37 +0100

    Initializing Hydrated Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:soundz_music_app/presentation/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
   runApp(MyApp());
 }
