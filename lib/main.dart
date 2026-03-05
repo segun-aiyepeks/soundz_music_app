2c28d812bc3c754f983115fd61e6e7c695627c97
segun-aiyepeks <your-email@example.com>
2026-03-05 12:25:55 +0100

    I implemented Firebase on the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:soundz_music_app/firebase_options.dart';
 import 'package:soundz_music_app/presentation/app.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorageDirectory.web
         : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
   );
   runApp(MyApp());
 }
